e62a63708306e9bd6b337e96cc32723c04b81a71
gmgsangeetha <gondesimohangeetikasangeetha@gmail.com>
2026-05-06 16:43:22 +0530

    Update DeleteStudentServlet.java

--- src/main/java/controller/DeleteStudentServlet.java
+++ src/main/java/controller/DeleteStudentServlet.java
@@ -1,5 +1,4 @@
 package controller;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
@@ -9,30 +8,21 @@ import java.sql.*;
 @WebServlet("/DeleteStudentServlet")
 public class DeleteStudentServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         String rollNo = request.getParameter("roll_no");
-
         try {
         	Connection con = DBConnection.getConnection();
             PreparedStatement pst = con.prepareStatement("DELETE FROM students WHERE roll_no=?");
             pst.setString(1, rollNo);
             int rowsAffected = pst.executeUpdate();
-
             if (rowsAffected > 0) {
                 request.getSession().setAttribute("message", "Roll number " + rollNo + " deleted successfully.");
-             
-            } else {
+            } 
+            else {
                 request.getSession().setAttribute("message", "Roll number " + rollNo + " is not yet added.");
             }
-
-          
             response.sendRedirect("dashboard1.jsp");
-
-            
-
         } catch (Exception e) {
             e.printStackTrace();
         }
