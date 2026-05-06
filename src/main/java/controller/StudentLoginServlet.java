a61d7f25eb56eed3460e4093fe1ff73e57692d5f
gmgsangeetha <gondesimohangeetikasangeetha@gmail.com>
2026-05-06 16:47:37 +0530

    Update StudentLoginServlet.java

--- src/main/java/controller/StudentLoginServlet.java
+++ src/main/java/controller/StudentLoginServlet.java
@@ -1,5 +1,4 @@
 package controller;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +8,6 @@ import java.io.IOException;
 import java.sql.*;
 import javax.servlet.http.*;
 
-
 @WebServlet("/StudentLoginServlet")
 public class StudentLoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -17,23 +15,18 @@ public class StudentLoginServlet extends HttpServlet {
             throws ServletException, IOException {
         String rollNo = request.getParameter("roll_no");
         String password = request.getParameter("password");
-
         try {
         	Connection con = DBConnection.getConnection();
-
-            // Check feedback access
             PreparedStatement accessStmt = con.prepareStatement("SELECT is_allowed FROM feedback_access WHERE id=1");
             ResultSet accessRs = accessStmt.executeQuery();
             if (accessRs.next() && !accessRs.getBoolean("is_allowed")) {
                 response.getWriter().println("Feedback submission is currently closed.");
                 return;
             }
-
             PreparedStatement pst = con.prepareStatement("SELECT * FROM students WHERE roll_no=? AND password=?");
             pst.setString(1, rollNo);
             pst.setString(2, password);
             ResultSet rs = pst.executeQuery();
-
             if (rs.next()) {
                 if (rs.getBoolean("feedback_submitted")) {
                     response.sendRedirect("already_submitted.jsp");
@@ -43,13 +36,10 @@ public class StudentLoginServlet extends HttpServlet {
                     response.sendRedirect("feedback.jsp");
                 }
             } else {
-            	
-            	
             	 response.getWriter().println("Invalid Roll Number or Password");
             }
-
             con.close();
-        } catch (Exception e) {
+            } catch (Exception e) {
             e.printStackTrace();
         }
     }
