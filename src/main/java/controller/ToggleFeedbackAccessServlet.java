afe55728b32f2e4c81ad002583a3a3c3bcb90847
gmgsangeetha <gondesimohangeetikasangeetha@gmail.com>
2026-05-06 16:52:56 +0530

    Update ToggleFeedbackAccessServlet.java

--- src/main/java/controller/ToggleFeedbackAccessServlet.java
+++ src/main/java/controller/ToggleFeedbackAccessServlet.java
@@ -1,6 +1,4 @@
 package controller;
-
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,27 +7,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.*;
 
-
 @WebServlet("/ToggleFeedbackAccessServlet")
 public class ToggleFeedbackAccessServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         String action = request.getParameter("action");
         boolean allow = "Allow".equalsIgnoreCase(action);
-
         try {
         	Connection con = DBConnection.getConnection();
-
             PreparedStatement pst = con.prepareStatement("UPDATE feedback_access SET is_allowed = ? WHERE id = 1");
             pst.setBoolean(1, allow);
             pst.executeUpdate();
-
             pst.close();
             con.close();
-        } catch (Exception e) {
+            } catch (Exception e) {
             e.printStackTrace();
         }
     }
